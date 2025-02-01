2d2c43c30ca4e53aa9e550a303acac5301292603
BugHunter-X <askzenai@gmail.com>
2025-02-01 07:38:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,16 @@
+function myFunc() {
+  var a = 10;
+  console.log(a); // Output: 10
+}
+
+//Alternative using let
+function myFuncLet(){
+  let a = 10;
+  console.log(a);
+}
+
+//Alternative using const
+function myFuncConst(){
+  const a = 10;
+  console.log(a);
+} 
